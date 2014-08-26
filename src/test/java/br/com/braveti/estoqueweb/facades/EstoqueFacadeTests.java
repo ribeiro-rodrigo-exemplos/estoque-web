5ad6c676e289eb0ed8d930bf550f9c650663a450
Rodrigo Ribeiro <rodrigo@braveti.com.br>
2014-08-26 19:29:03 -0300

    implementacao dos testes da classe EstoqueFacade utilizando o framework mockito

--- src/test/java/br/com/braveti/estoqueweb/facades/EstoqueFacadeTests.java
+++ src/test/java/br/com/braveti/estoqueweb/facades/EstoqueFacadeTests.java
@@ -1,118 +1,174 @@
 package br.com.braveti.estoqueweb.facades;
 
-import org.junit.After;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 
 import static org.junit.Assert.*; 
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import static org.mockito.Mockito.*;
+import org.mockito.*;
+import org.mockito.MockitoAnnotations;
 
-import br.com.braveti.estoqueweb.dao.ConexaoBancoDeDados;
 import br.com.braveti.estoqueweb.dao.DAOException;
-import br.com.braveti.estoqueweb.dao.DataSource;
 import br.com.braveti.estoqueweb.dao.EstoqueDAO;
-import br.com.braveti.estoqueweb.dao.FabricaDeSessao;
-import br.com.braveti.estoqueweb.dao.FabricaDeSessaoImpl;
 import br.com.braveti.estoqueweb.dominio.Estoque;
-import br.com.braveti.estoqueweb.facades.EstoqueFacade;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 public class EstoqueFacadeTests {
 
 	private EstoqueFacade facade;
-	private static FabricaDeSessao fabricaDeSessao; 
+
+    @Mock
 	private EstoqueDAO dao; 
-	
-	@BeforeClass
-	public static void antesDeTudo(){
-		
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml"); 
-		
-		ConexaoBancoDeDados conexao = context.getBean("testeConexao",ConexaoBancoDeDados.class);
-		DataSource dataSource = new DataSource(conexao); 
-		fabricaDeSessao = new FabricaDeSessaoImpl(dataSource);
-		
-	}
-	
+
 	@Before
 	public void antes(){
 		
-		dao = new EstoqueDAO(fabricaDeSessao); 
-		facade = new EstoqueFacade(dao); 
-		dao.iniciarTransacao(); 
-		
+        MockitoAnnotations.initMocks(this);
+		facade = new EstoqueFacade(dao);
 	}
 	
-	@After
-	public void depois(){
 
-        try{
-
-		dao.finalizarTransacao();
-
-        }catch(DAOException e){
-
-            e.printStackTrace();
-        }
-	}
-	
 	@Test
-	public void testDeveCadastrarEstoque()throws DAOException{
-		
-		Estoque estoque = gerarEstoque(); 
-		
-		Long id = facade.criarEstoque(estoque); 
+	public void deveCadastrarEstoque()throws DAOException{
+
+        Estoque estoque = gerarEstoque();
+
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocationOnMock) throws Throwable {
+
+                Estoque estoque = (Estoque) invocationOnMock.getArguments()[0];
+                estoque.setId(1L);
+                return null;
+            }
+
+        }).when(dao).salvar(estoque);
+
+		Long id = facade.criarEstoque(estoque);
+
+        InOrder ordem = inOrder(dao);
+
+        ordem.verify(dao).iniciarTransacao();
+        ordem.verify(dao).salvar(estoque);
+        ordem.verify(dao).finalizarTransacaoEFecharConexao();
 		
 		assertNotNull(id); 
-		assertEquals(estoque.getId(),id); 
-		 
-		Estoque estoquePesquisado = dao.buscar(Estoque.class, id); 
-		
-		assertNotNull(estoquePesquisado); 
+		assertEquals(1L,id);
+
 	}
+
+    @Test(expected=DAOException.class)
+    public void testDisparoDeExcecaoNoCadastroDoEstoque()throws DAOException{
+
+        Estoque estoque = gerarEstoque();
+
+        doThrow(DAOException.class).when(dao).salvar(estoque);
+
+        facade.criarEstoque(estoque);
+
+    }
 	
 	@Test
-	public void testDeveRemoverEstoque()throws DAOException{
+	public void deveRemoverEstoque()throws DAOException{
 		
-		Estoque estoque = gerarEstoque(); 
-		
-		Long id = facade.criarEstoque(estoque); 
-		
-		Estoque estoquePesquisado = dao.buscar(Estoque.class, id); 
-		
-		assertNotNull(estoquePesquisado); 
-		
-		facade.removerEstoque(estoquePesquisado);
-		
-		Estoque estoqueRemovido = dao.buscar(Estoque.class, id); 
-		
-		assertNull(estoqueRemovido); 
+		Estoque estoquePesquisado = gerarEstoque();
+        Estoque estoqueRemovido = gerarEstoque();
+
+        estoqueRemovido.setId(1L);
+
+        when(dao.buscar(Estoque.class,estoqueRemovido.getId())).thenReturn(estoquePesquisado);
+
+        facade.removerEstoque(estoqueRemovido);
+
+        InOrder ordem = inOrder(dao);
+
+        ordem.verify(dao).buscar(Estoque.class,estoqueRemovido.getId());
+        ordem.verify(dao).iniciarTransacao();
+        ordem.verify(dao).remover(estoquePesquisado);
+        ordem.verify(dao).finalizarTransacao();
+        ordem.verify(dao).fecharConexao();
+
 	}
+
+    @Test(expected=DAOException.class)
+    public void testDisparoDeExcecaoNaRemocaoDoEstoque()throws DAOException{
+
+        Estoque estoque = gerarEstoque();
+        estoque.setId(1L);
+
+        when(dao.buscar(Estoque.class,estoque.getId())).thenReturn(estoque);
+
+        doThrow(DAOException.class).when(dao).finalizarTransacao();
+
+        facade.removerEstoque(estoque);
+
+    }
 	
 	@Test
-	public void testDeveAlterarEstoque()throws DAOException{
-		
-		String novoNome = "estoque-leitores"; 
-		
-		Estoque estoque = gerarEstoque(); 
-		
-		Long id = facade.criarEstoque(estoque); 
-		
-		Estoque estoquePesquisado = dao.buscar(Estoque.class,id); 
-		
-		assertNotNull(estoquePesquisado); 
-		
-		estoquePesquisado.setNome(novoNome); 
-		
-		facade.alterarEstoque(estoquePesquisado);
-		
-		Estoque estoqueAlterado = dao.buscar(Estoque.class, id); 
-		
-		assertEquals(novoNome,estoqueAlterado.getNome()); 
+	public void deveAlterarEstoque()throws DAOException{
+
+        Estoque estoqueAlterado = gerarEstoque();
+        Estoque estoquePesquisado = mock(Estoque.class);
+
+        estoqueAlterado.setId(1L);
+
+        when(dao.buscar(Estoque.class,estoqueAlterado.getId())).thenReturn(estoquePesquisado);
+
+        facade.alterarEstoque(estoqueAlterado);
+
+        ArgumentCaptor<String> nome = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> descricao = ArgumentCaptor.forClass(String.class);
+
+        verify(estoquePesquisado).setNome(nome.capture());
+        verify(estoquePesquisado).setDescricao(descricao.capture());
+
+        InOrder ordem = inOrder(dao);
+
+        ordem.verify(dao).iniciarTransacao();
+        ordem.verify(dao).alterar(estoquePesquisado);
+        ordem.verify(dao).finalizarTransacao();
+        ordem.verify(dao).fecharConexao();
+
+        assertEquals(estoqueAlterado.getNome(),nome.getValue());
+        assertEquals(estoqueAlterado.getDescricao(),descricao.getValue());
+
 		
 	}
+
+    @Test(expected=DAOException.class)
+    public void testDisparoDeExcecaoNaAlteracaoDoEstoque()throws DAOException{
+
+        Estoque estoquePesquisado = gerarEstoque();
+        Estoque estoqueAlterado = gerarEstoque();
+        estoqueAlterado.setId(1L);
+
+        when(dao.buscar(Estoque.class,estoqueAlterado.getId())).thenReturn(estoquePesquisado);
+
+        doThrow(DAOException.class).when(dao).finalizarTransacao();
+
+        facade.removerEstoque(estoqueAlterado);
+
+
+    }
+
+    @Test
+    public void testRemocaoDeEstoqueInexistente()throws DAOException{
+
+        Estoque estoque = gerarEstoque();
+        estoque.setId(1L);
+
+        when(dao.buscar(Estoque.class,estoque.getId())).thenReturn(null);
+
+        facade.removerEstoque(estoque);
+
+        verify(dao,never()).iniciarTransacao();
+        verify(dao,never()).remover(estoque);
+        verify(dao,never()).finalizarTransacao();
+        verify(dao).fecharConexao();
+    }
 	
 	private Estoque gerarEstoque(){
 		
